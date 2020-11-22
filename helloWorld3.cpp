62f6ed03efeb86c377f348a25602bbe2887d7aa9
Ubuntu <ubuntu@ip-172-31-14-246.ec2.internal>
2020-11-22 03:43:20 +0000

    cpp proper style

---
+++ helloWorld3.cpp
@@ -0,0 +1,11 @@
+// copyright (c) 2020 Ahmad All rights reserved  
+//
+// created by Ahmad El-khawaldeh
+// created on Nov 2020
+// This is "hello world " program with proper style
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!" << std::endl;
+}
